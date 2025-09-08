31b7c8259c23f006bd5519e552ad47a123276abd
AlexSD <jesus2209.isc@gmail.com>
2025-09-07 18:38:32 -0600

    Se dan de alta las entidades a AplicattionDbContext

--- Proyecto3/Data/Migration/ApplicationDbContext.cs
+++ Proyecto3/Data/Migration/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Proyecto3.Models;
 
 namespace Proyecto3.Data.Migration
 {
@@ -6,5 +7,12 @@ namespace Proyecto3.Data.Migration
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
+        public DbSet<Clientes> Clientes { get; set; }
+        public DbSet<Contactos> Contactos { get; set; }
+        public DbSet<Correos> Correos { get; set; }
+        public DbSet<Direcciones> Direcciones { get; set; }
+        public DbSet<Estados> Estados { get; set; }
+        public DbSet<Municipios> Municipios { get; set; }
+        public DbSet<Seguimientos> Seguimientos { get; set; }
     }
 }
