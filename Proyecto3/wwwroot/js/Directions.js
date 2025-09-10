d554789ae038cf4629fdc342bb3f2d0598d9483e
AlexSD <jesus2209.isc@gmail.com>
2025-09-10 12:12:34 -0600

    Mejoras de interface, se adapto a modo oscuro

---
+++ Proyecto3/wwwroot/js/Directions.js
@@ -0,0 +1,24 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+    $(".ver-detalle").click(function () {
+        var id = $(this).data("id");
+
+        $.ajax({
+            url: urlDetailsPartial,
+            data: { id: id },
+            type: "GET",
+            success: function (result) {
+                $("#detalleModalBody").html(result);
+                $("#detalleModal").modal("show");
+            },
+            error: function () {
+                alert("Error al cargar los detalles.");
+            }
+        });
+
+    });
+});
--- Proyecto3/wwwroot/js/site.js
+++ Proyecto3/wwwroot/js/site.js
@@ -2,3 +2,31 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    //Se le asigna la clase toogle
+    const toggleButton = document.getElementById("toggle-dark");
+
+    //body del html
+    const body = document.body;
+
+    // Se guarda en locals
+    if (localStorage.getItem("theme") === "dark") {
+        body.classList.add("dark-mode");
+    }
+
+    toggleButton.addEventListener("click", function () {
+        body.classList.toggle("dark-mode");
+
+        if (body.classList.contains("dark-mode")) {
+            toggleButton.textContent = "Modo claro";
+            localStorage.setItem("theme", "dark");
+            
+        } else {
+            toggleButton.textContent = "Modo oscuro";
+            localStorage.setItem("theme", "light");
+        }
+    });
+});
